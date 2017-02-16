9a830da0f17b1c050b5dee0ddb9d3f9ac8f83cc4
jonathanlarsen <jal@atol.dk>
2017-02-16 09:05:34 +0100

    TUI class updated

--- CDIO1/src/tui/TUI.java
+++ CDIO1/src/tui/TUI.java
@@ -1,7 +1,50 @@
 package tui;
 
+import java.util.Scanner;
+
 /**
  * Created by jonathanlarsen on 14/02/2017.
  */
 public class TUI {
-}
+
+        int input;
+
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.println("============================");
+            System.out.println("|       MENU SELECTION     |");
+            System.out.println("============================");
+            System.out.println("| Options:                 |");
+            System.out.println("|        1. Create User    |");
+            System.out.println("|        2. Update User    |");
+            System.out.println("|        3. Delete User    |");
+            System.out.println("|        4. List Users     |");
+            System.out.println("|        5. Exit           |");
+            System.out.println("============================");
+
+            input = scan.nextInt();
+
+            switch (input) {
+                case 1:
+                    System.out.println("Create User");
+                    break;
+                case 2:
+                    System.out.println("Update User");
+                    break;
+                case 3:
+                    System.out.println("Delete User");
+                    break;
+                case 4:
+                    System.out.println("List Users");
+                    break;
+                case 5:
+                    System.out.println("System closed");
+                    break;
+                default:
+                    System.out.println("Invalid entry");
+                    break;
+            }
+
+        } while (input != 5);
+    }
